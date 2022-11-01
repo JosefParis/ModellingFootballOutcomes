a7f8eaa7d70f2ac3927b81218db33f761945b756
JoeyParisOHG <JosefParis@openhealthgroup.com>
2022-11-01 20:53:29 +0000

    Early model work

---
+++ Early models.R
@@ -0,0 +1,39 @@
+if(!require('stats')) {
+  install.packages('stats')
+  library('stats')
+}
+
+# Model fitting:
+# Model cleaning and manipulation
+stats_per_game <- read.csv('understat_per_game.csv')
+stats_league <- read.csv('understat.com.csv')
+EPL <- stats_per_game[stats_per_game$league == 'EPL',]
+EPL <- mutate(EPL, home = case_when(h_a == 'h'  ~ '1',
+                          h_a == 'a' ~ '0'))
+
+
+# Linear model
+prediciton_matrix <- unique(EPL[, c('team', 'home')])
+
+linear_model_xg <- glm(xG ~ team + home, family = gaussian, data = EPL)
+summary(linear_model_xg)
+
+linear_model_xga <- glm(xGA ~ team + home, family = gaussian, data = EPL)
+summary(linear_model_xga)
+
+prediciton_xg <- predict(linear_model_xg, prediciton_matrix)
+prediciton_xga <- predict(linear_model_xga, prediciton_matrix)
+  
+prediciton_matrix$xg <- prediciton_xg
+prediciton_matrix$xga <- prediciton_xga
+
+# Poisson extraction (P(Goals| xG))
+
+for (i in 0:max(EPL$scored)){
+  column_index <- grep("^xG$", colnames(EPL))
+  temp_dataset <- subset(EPL, scored == i)
+  print(mean(temp_dataset[,column_index]))
+  xg_means <- list()
+  xg_means[i] <- mean(temp_dataset[,column_index]) 
+  rm(temp_dataset)
+}
