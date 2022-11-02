19af8526ba929893ba7aca16524cbe0bcbad3f52
JosefParis <josef.paris@ntlworld.com>
2022-11-02 11:58:49 +0000

    Early models: update to data cleaning process addition of season column

--- Early models.R
+++ Early models.R
@@ -2,15 +2,29 @@ if(!require('stats')) {
   install.packages('stats')
   library('stats')
 }
+if(!require('dplyr')) {
+  install.packages('dplyr')
+  library('dplyr')
+}
 
 # Model fitting:
 # Model cleaning and manipulation
 stats_per_game <- read.csv('understat_per_game.csv')
 stats_league <- read.csv('understat.com.csv')
+
 EPL <- stats_per_game[stats_per_game$league == 'EPL',]
+
 EPL <- mutate(EPL, home = case_when(h_a == 'h'  ~ '1',
                           h_a == 'a' ~ '0'))
 
+EPL <- mutate(EPL, season = case_when(year == '2014' ~ '2014/15',
+                                      year == '2015' ~ '2015/16',
+                                      year == '2016' ~ '2016/17',
+                                      year == '2017' ~ '2017/18',
+                                      year == '2018' ~ '2018/19',
+                                      year == '2019' ~ '2019/20'))
+
+EPL$GW <- unlist(sapply(table(EPL$year), seq))
 
 # Linear model
 prediciton_matrix <- unique(EPL[, c('team', 'home')])
@@ -27,6 +41,7 @@ prediciton_xga <- predict(linear_model_xga, prediciton_matrix)
 prediciton_matrix$xg <- prediciton_xg
 prediciton_matrix$xga <- prediciton_xga
 
+rm('prediciton_xg','prediciton_xga')
 # Poisson extraction (P(Goals| xG))
 
 for (i in 0:max(EPL$scored)){
